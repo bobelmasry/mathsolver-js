5968004c8e38586ba95126d8fd4933eba6c6c750
bobelmasry <alyhassan2254@gmail.com>
2024-04-10 14:42:35 +0200

    updated port and made https

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,7 +19,7 @@ export default function Home() {
       const formData = new FormData();
       formData.append('image', file);
 
-      const response = await fetch('http://41.43.83.152:8000/api/upload-image/', {
+      const response = await fetch('https://41.43.83.152:443/api/upload-image/', {
         method: 'POST',
         body: formData
       });
